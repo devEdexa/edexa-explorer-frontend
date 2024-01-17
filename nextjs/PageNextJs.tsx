70c8da766ab381db00ea0b19b06220a561d55d87
Viral Brahmbhatt <v.brahmbhatt@edexa.team>
2024-01-17 18:17:56 +0530

    fix:Meta tag URL

--- nextjs/PageNextJs.tsx
+++ nextjs/PageNextJs.tsx
@@ -1,17 +1,17 @@
-import Head from 'next/head';
-import React from 'react';
+import Head from "next/head";
+import React from "react";
 
-import type { Route } from 'nextjs-routes';
+import type { Route } from "nextjs-routes";
 
-import useAdblockDetect from 'lib/hooks/useAdblockDetect';
-import useGetCsrfToken from 'lib/hooks/useGetCsrfToken';
-import * as metadata from 'lib/metadata';
-import * as mixpanel from 'lib/mixpanel';
-import { init as initSentry } from 'lib/sentry/config';
+import useAdblockDetect from "lib/hooks/useAdblockDetect";
+import useGetCsrfToken from "lib/hooks/useGetCsrfToken";
+import * as metadata from "lib/metadata";
+import * as mixpanel from "lib/mixpanel";
+import { init as initSentry } from "lib/sentry/config";
 
 type Props = Route & {
   children: React.ReactNode;
-}
+};
 
 initSentry();
 
@@ -27,18 +27,22 @@ const PageNextJs = (props: Props) => {
   return (
     <>
       <Head>
-        <title>{ title }</title>
-        <meta name="description" content={ description }/>
-
-        { /* OG TAGS */ }
-        <meta property="og:title" content={ opengraph.title }/>
-        { opengraph.description && <meta property="og:description" content={ opengraph.description }/> }
-        <meta property="og:image" content={ opengraph.imageUrl }/>
-        <meta name="twitter:card" content="summary_large_image"/>
-        <meta property="twitter:image" content={ opengraph.imageUrl }/>
-        <meta property="og:type" content="website"/>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>edeXa Business Blockchain</title>
+        <meta property="og:title" content="edeXa Business Blockchain" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://explorer.testnet.edexa.com/" />
+        <meta
+          property="og:image"
+          content="https://edexa.network/images/thumnail_edeXa.png"
+        />
+        <meta
+          property="og:description"
+          content="edeXa Business Blockchain, private and public ecosystem built to enable scalable and business- oriented dApps for the world."
+        />
       </Head>
-      { props.children }
+      {props.children}
     </>
   );
 };
