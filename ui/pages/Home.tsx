0ffba8b20bc5432107c548b4a79635e71a18aaa0
shariff-edx <n.shaik@edexa.team>
2024-06-21 15:04:02 +0530

    updated code

--- ui/pages/Home.tsx
+++ ui/pages/Home.tsx
@@ -1,17 +1,14 @@
-import { Box, Heading, Flex, LightMode } from "@chakra-ui/react";
-import React from "react";
+import { Box, Heading, Flex, LightMode } from '@chakra-ui/react';
+import React from 'react';
 
-import config from "configs/app";
-import ChainIndicators from "ui/home/indicators/ChainIndicators";
-import LatestBlocks from "ui/home/LatestBlocks";
-import LatestZkEvmL2Batches from "ui/home/LatestZkEvmL2Batches";
-import Stats from "ui/home/Stats";
-import Transactions from "ui/home/Transactions";
+import config from 'configs/app';
+import LatestBlocks from 'ui/home/LatestBlocks';
+import LatestZkEvmL2Batches from 'ui/home/LatestZkEvmL2Batches';
+import Stats from 'ui/home/Stats';
+import Transactions from 'ui/home/Transactions';
 // import AdBanner from 'ui/shared/ad/AdBanner';
-import ProfileMenuDesktop from "ui/snippets/profileMenu/ProfileMenuDesktop";
-import SearchBar from "ui/snippets/searchBar/SearchBar";
-import AdBanner from "ui/shared/ad/AdBanner";
-import ConnectWalletButton from "ui/shared/ConnectWalletButton";
+import ConnectWalletButton from 'ui/shared/ConnectWalletButton';
+import SearchBar from 'ui/snippets/searchBar/SearchBar';
 
 const rollupFeature = config.features.rollup;
 
@@ -20,10 +17,10 @@ const Home = () => {
     <Box as="main">
       <Box
         w="100%"
-        background={config.UI.homepage.plate.background}
+        background={ config.UI.homepage.plate.background }
         borderRadius="24px"
-        padding={{ base: "24px", lg: "48px" }}
-        minW={{ base: "unset", lg: "900px" }}
+        padding={{ base: '24px', lg: '48px' }}
+        minW={{ base: 'unset', lg: '900px' }}
         data-label="hero plate"
       >
         <Flex
@@ -33,24 +30,24 @@ const Home = () => {
         >
           <Heading
             as="h1"
-            size={{ base: "md", lg: "xl" }}
-            lineHeight={{ base: "32px", lg: "50px" }}
-            fontWeight={600}
-            color={config.UI.homepage.plate.textColor}
+            size={{ base: 'md', lg: 'xl' }}
+            lineHeight={{ base: '32px', lg: '50px' }}
+            fontWeight={ 600 }
+            color={ config.UI.homepage.plate.textColor }
           >
-            Welcome to {config.chain.name} explorer
+            Welcome to { config.chain.name } explorer
           </Heading>
-          <Box display={{ base: "none", lg: "block" }}>
-            {/* {config.features.account.isEnabled && <ProfileMenuDesktop />} */}
-            <ConnectWalletButton />
+          <Box display={{ base: 'none', lg: 'block' }}>
+            { /* {config.features.account.isEnabled && <ProfileMenuDesktop />} */ }
+            <ConnectWalletButton/>
           </Box>
         </Flex>
         <LightMode>
-          <SearchBar isHomepage />
+          <SearchBar isHomepage/>
         </LightMode>
       </Box>
-      <Stats />
-      <Heading
+      <Stats/>
+      { /* <Heading
         as="h5"
         size="sm"
         mb={4}
@@ -63,29 +60,29 @@ const Home = () => {
       >
         Note: We're indexing this chain right now. Some of the counts may be
         inaccurate.
-      </Heading>
-      {/* <ChainIndicators /> */}
-      {/* <AdBanner
+      </Heading> */ }
+      { /* <ChainIndicators /> */ }
+      { /* <AdBanner
         mt={{ base: 6, lg: 8 }}
         mx="auto"
         display="flex"
         justifyContent="center"
-      /> */}
+      /> */ }
 
-      {/* hiding for presale banner */}
+      { /* hiding for presale banner */ }
       <Flex
-        mt={8}
-        direction={{ base: "column", lg: "row" }}
-        columnGap={12}
-        rowGap={8}
+        mt={ 8 }
+        direction={{ base: 'column', lg: 'row' }}
+        columnGap={ 12 }
+        rowGap={ 8 }
       >
-        {rollupFeature.isEnabled && rollupFeature.type === "zkEvm" ? (
-          <LatestZkEvmL2Batches />
+        { rollupFeature.isEnabled && rollupFeature.type === 'zkEvm' ? (
+          <LatestZkEvmL2Batches/>
         ) : (
-          <LatestBlocks />
-        )}
-        <Box flexGrow={1}>
-          <Transactions />
+          <LatestBlocks/>
+        ) }
+        <Box flexGrow={ 1 }>
+          <Transactions/>
         </Box>
       </Flex>
     </Box>
